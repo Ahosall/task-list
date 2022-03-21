1b11a721e45f89870448a92a5c757971fb3d8b48
Ahosall <zen.wixyos.br@gmail.com>
2022-03-21 16:26:46 -0400

    Estrutura

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+import Button from './Button.jsx'
+
+import './AddTask.css'
+
+const AddTask = ({ handleTaskAddition }) => {
+  const [inputData, setInputData] = useState("")
+
+  const handleInputChange = (e) => {
+    setInputData(e.target.value);
+  }
+
+  const handleAddTaskClick = () => {
+    if (inputData.trim().length === 0) return
+    handleTaskAddition(inputData);
+    setInputData("");
+  }
+
+  return (
+    <div className="add-task-container">
+      <input
+        onChange={handleInputChange}
+        className="add-task-input"
+        value={inputData}
+        type="text"
+      />
+      <div className="add-task-button-container">
+        <Button onClick={handleAddTaskClick}>Adicionar</Button>
+      </div>
+    </div>
+  )
+}
+
+export default AddTask;
